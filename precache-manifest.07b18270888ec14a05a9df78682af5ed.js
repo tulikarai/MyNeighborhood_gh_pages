cb6b48294a9919be5cee7e9055de3ed6f3e420ba
tulika.rai <tulika.rai@gmail.com>
2018-12-01 00:08:56 -0700

    Updates

---
+++ precache-manifest.07b18270888ec14a05a9df78682af5ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ad4fa30ebab969446d",
+    "url": "/MyNeighborhood_gh_pages/static/js/runtime~main.e4ad4fa3.js"
+  },
+  {
+    "revision": "136247fd55bb15e68e94",
+    "url": "/MyNeighborhood_gh_pages/static/js/main.136247fd.chunk.js"
+  },
+  {
+    "revision": "78c22aa3721d21b6404e",
+    "url": "/MyNeighborhood_gh_pages/static/js/1.78c22aa3.chunk.js"
+  },
+  {
+    "revision": "136247fd55bb15e68e94",
+    "url": "/MyNeighborhood_gh_pages/static/css/main.65c821d7.chunk.css"
+  },
+  {
+    "revision": "af75b2ce6791708f1e0dda917351eefa",
+    "url": "/MyNeighborhood_gh_pages/index.html"
+  }
+];
